a1e5aaff473d069e8e4e93edb342ad460f6b5dd4
Mike Bjorkegren <m@bjorkegren.com>
2016-05-15 03:26:53 -0700

    add copyright notice to all files

--- postgres/db_setup_draft.sql
+++ postgres/db_setup_draft.sql
@@ -1,3 +1,11 @@
+-- Copyright 2012-present, Polis Technology Inc.
+-- All rights reserved.
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree. An additional grant
+-- of patent rights for non-commercial use can be found in the PATENTS file
+-- in the same directory.
+
+
 -- NOTE: use \d TABLENAME to see info, like indexes
 
 -- Intersting query examples:
