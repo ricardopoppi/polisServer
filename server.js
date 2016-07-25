c50dbdb7f9a44842e4b6057642c6d301ef2f1ca6
Mike Bjorkegren <m@bjorkegren.com>
2016-07-25 16:48:54 -0700

    wip slack auth

--- server.js
+++ server.js
@@ -2668,6 +2668,10 @@ function initializePolisHelpers(mongoParams) {
     return server;
   }
 
+  function handle_POST_auth_slack_redirect_uri(req, res) {
+    console.dir(req);
+    res.status(200).send("");
+  }
 
 
   function handle_POST_auth_pwresettoken(req, res) {
@@ -12084,6 +12088,7 @@ CREATE TABLE slack_user_invites (
     handle_POST_auth_new,
     handle_POST_auth_password,
     handle_POST_auth_pwresettoken,
+    handle_POST_auth_slack_redirect_uri,
     handle_POST_comments,
     handle_POST_contexts,
     handle_POST_contributors,
