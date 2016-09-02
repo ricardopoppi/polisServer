81a1ffd68d2859c54f52ef6bf71e23438ee43819
Mike Bjorkegren <m@bjorkegren.com>
2016-09-02 16:22:02 -0700

    add is_seed column to comments

--- app.js
+++ app.js
@@ -623,12 +623,12 @@ helpersInitialized.then(function(o) {
     need('conversation_id', getConversationIdFetchZid, assignToPCustom('zid')),
     want('txt', getOptionalStringLimitLength(997), assignToP),
     want('vote', getIntInRange(-1, 1), assignToP, -1), // default to agree
-    want('prepop', getBool, assignToP),
     want("twitter_tweet_id", getStringLimitLength(999), assignToP),
     want("quote_twitter_screen_name", getStringLimitLength(999), assignToP),
     want("quote_txt", getStringLimitLength(999), assignToP),
     want("quote_src_url", getUrlLimitLength(999), assignToP),
     want("anon", getBool, assignToP),
+    want("is_seed", getBool, assignToP),
     resolve_pidThing('pid', assignToP, "post:comments"),
     handle_POST_comments);
 
@@ -639,12 +639,12 @@ helpersInitialized.then(function(o) {
     need('conversation_id', getConversationIdFetchZid, assignToPCustom('zid')),
     want('txt', getOptionalStringLimitLength(997), assignToP),
     want('vote', getIntInRange(-1, 1), assignToP, -1), // default to agree
-    want('prepop', getBool, assignToP),
     want("twitter_tweet_id", getStringLimitLength(999), assignToP),
     want("quote_twitter_screen_name", getStringLimitLength(999), assignToP),
     want("quote_txt", getStringLimitLength(999), assignToP),
     want("quote_src_url", getUrlLimitLength(999), assignToP),
     want("anon", getBool, assignToP),
+    want("is_seed", getBool, assignToP),
     resolve_pidThing('pid', assignToP, "post:comments"),
     handle_POST_comments_slack);
 
